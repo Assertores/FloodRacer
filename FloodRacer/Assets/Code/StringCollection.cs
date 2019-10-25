ef3059c8ead11703f3ae27b0de882dad562aba09
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-25 23:23:14 +0200

    [PRO] created StringCollection

---
+++ FloodRacer/Assets/Code/StringCollection.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class StringCollection {
+
+	public const string A_HORIZONTAL = "Horizontal";
+	public const string A_VERTICAL = "Vertical";
+}
