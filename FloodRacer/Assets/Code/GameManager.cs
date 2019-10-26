8ae74c92ea9e900eca967dc007cc94a19d07c192
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-26 13:16:28 +0200

    [PRO] First GameDesigner Level added as start level

--- FloodRacer/Assets/Code/GameManager.cs
+++ FloodRacer/Assets/Code/GameManager.cs
@@ -33,8 +33,6 @@ public class GameManager : Singleton<GameManager> {
 			return;
 		}
 
-		DontDestroyOnLoad(this.gameObject);
-
 		GameObject tmp = Instantiate(p_startLevel);
 		m_currentLevel = CheckNextLevel(tmp);
 
