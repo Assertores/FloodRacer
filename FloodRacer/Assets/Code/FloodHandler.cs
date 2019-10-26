2b3f4caa58536eafbdb849683171ff605a176412
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-26 16:46:57 +0200

    [PRO] flood chaises Player

--- FloodRacer/Assets/Code/FloodHandler.cs
+++ FloodRacer/Assets/Code/FloodHandler.cs
@@ -5,13 +5,16 @@ using UnityEngine;
 public class FloodHandler : MonoBehaviour {
 
 	[SerializeField] AnimationCurve m_acelleration;
+	[SerializeField] float m_maxDistToPlayer = 100;
 
 	private void Update() {
+		if(Mathf.Abs(Controle.s_instance.transform.position.z - transform.position.z) > m_maxDistToPlayer) {
+			transform.position = new Vector3(0, 0, Controle.s_instance.transform.position.z - m_maxDistToPlayer);
+		}
 		transform.position += new Vector3(0, 0, m_acelleration.Evaluate(Time.timeSinceLevelLoad) * Time.deltaTime);
 	}
 
 	private void OnTriggerEnter(Collider other) {
-		print("1 " + other.tag + " : " + other.transform.root.tag);
 		if(other.transform.root.tag == StringCollection.T_PLAYER) {
 			if(MenuHandler.Exists())
 				MenuHandler.s_instance.FinishLevel(Controle.s_instance.transform.position.z);
