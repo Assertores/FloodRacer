4c168ed2eb9ce342463c777b6280113fd7353674
[JAM] Andreas <andreas.edmeier@web.de>
2019-10-26 13:04:33 +0200

    [PRO] added LevelHandler

---
+++ FloodRacer/Assets/Code/LevelHandler.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelHandler : MonoBehaviour {
+
+	public GameObject[] p_nextLevels;
+	public float m_levelLength;
+}
